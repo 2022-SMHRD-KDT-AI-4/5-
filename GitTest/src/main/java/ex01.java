96dee25a04f73b0df8a4c1aff6dd430ef1dbfc72
SM2201BB <SM2201BB@DESKTOP-28N4H5S>
2022-04-15 16:32:15 +0900

    세번째 수정

--- GitTest/src/main/java/ex01.java
+++ GitTest/src/main/java/ex01.java
@@ -4,7 +4,8 @@ public class ex01 {
 	public static void main(String[] args) {
 		
 		System.out.println("첫번째 커밋");
-		System.out.println("두번째 커밋  ");
+		System.out.println("두번째 커밋");
+		System.out.println("세번째 커밋");
 	}
 
 }
