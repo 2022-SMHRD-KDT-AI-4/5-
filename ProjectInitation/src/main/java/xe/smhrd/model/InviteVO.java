154918cdcdcb87ebc470815d53645d4d544354e1
김용태 <kytx@naver.com>
2022-05-03 15:26:31 +0900

    초대장 insert 각종 입력기능 추가 v1

---
+++ ProjectInitation/src/main/java/xe/smhrd/model/InviteVO.java
@@ -0,0 +1,23 @@
+package xe.smhrd.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@RequiredArgsConstructor
+public class InviteVO {
+	@NonNull
+	private int v_id;
+	private String m_id;
+	private String v_name;
+	private String v_wdate;
+	private String v_date;
+	private String v_cont;
+	private String p_id;
+	private String i_id;
+}
